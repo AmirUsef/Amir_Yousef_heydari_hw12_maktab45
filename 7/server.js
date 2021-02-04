1ed297e4989d34f07f51afaeba6f8b969c821615
Amir Yousef Heydari <ayh1380@gmail.com>
2021-02-04 10:37:12 +0330

    question 7 modified

--- 7/server.js
+++ 7/server.js
@@ -1,8 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 const users = require("./users");
 
 http.createServer(function(request, response) {
-    if (request.url === '/' && request.method === 'POST') {
+    if (request.url === '/' && request.method === 'GET') {
+        fs.readFile("./public/index.html", "utf8", (err, html) => {
+            if (err) console.log(err.message);
+            response.setHeader('Content-Type', 'text/html');
+            response.write(html);
+            response.end();
+        })
+    } else if (request.url === '/css/style.css' && request.method === 'GET') {
+        fs.readFile("./public/css/style.css", "utf8", (err, css) => {
+            if (err) console.log(err.message);
+            response.write(css);
+            response.end();
+        })
+    } else if (request.url === '/js/main.js' && request.method === 'GET') {
+        fs.readFile("./public/js/main.js", "utf8", (err, js) => {
+            if (err) console.log(err.message);
+            response.write(js);
+            response.end();
+        })
+    } else if (request.url === '/css/persian-gulf.jpg' && request.method === 'GET') {
+        fs.readFile("./public/css/persian-gulf.jpg", (err, data) => {
+            if (err) console.log(err.message);
+            response.writeHead(200, { 'Content-Type': 'image/jpeg' })
+            response.end(data);
+        })
+    } else if (request.url === '/' && request.method === 'POST') {
         let body = [];
         request.on('error', (err) => {
             console.error(err);
